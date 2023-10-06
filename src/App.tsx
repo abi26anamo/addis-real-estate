2b944c911339e15ea4316fb06b4e0bcca433c7c2
abi26anamo <abianamo282@gmail.com>
2023-10-06 09:48:42 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Logout from './pages/logout';
+import Profile from './pages/profile';
+
+
 const App = () => {
   return (
-    <div className="text-red-500 font-bold text-center">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
