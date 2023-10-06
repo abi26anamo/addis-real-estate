e6dd65c8d12b5b9f8e2d8c4d70efd0a7cee441c2
abi26anamo <abianamo282@gmail.com>
2023-10-06 10:17:54 +0300

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Logout from './pages/logout';
 import Profile from './pages/profile';
+import Header from './components/Header';
+import About from './pages/About';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/logout' element={<Logout />}></Route>
         <Route path='/profile' element={<Profile/>}></Route>
-
-
       </Routes>
     </BrowserRouter>
   )
